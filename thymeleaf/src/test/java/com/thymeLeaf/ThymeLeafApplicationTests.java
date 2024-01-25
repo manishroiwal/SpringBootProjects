f17c22555b9bcfb61c03c2f376f248c1c8c3ef93
manishroiwal <157649712+manishroiwal@users.noreply.github.com>
2024-01-26 01:23:29 +0530

    thymleafProject

---
+++ thymeleaf/src/test/java/com/thymeLeaf/ThymeLeafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thymeLeaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeLeafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
