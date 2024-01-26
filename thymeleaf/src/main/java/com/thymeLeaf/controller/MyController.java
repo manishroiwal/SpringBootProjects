586b95aaa9510ae38ee5c695868270579bf1911a
manishroiwal <157649712+manishroiwal@users.noreply.github.com>
2024-01-27 02:10:40 +0530

    new handler and condionaland iterate html pages

--- thymeleaf/src/main/java/com/thymeLeaf/controller/MyController.java
+++ thymeleaf/src/main/java/com/thymeLeaf/controller/MyController.java
@@ -1,10 +1,14 @@
 package com.thymeLeaf.controller;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Date;
-
+import java.util.List;
 import org.apache.tomcat.util.http.ConcurrentDateFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -23,4 +27,45 @@ public class MyController {
 		return "about";
 		
 	}
+	
+	// itarate handler
+	@GetMapping("/for-loop")
+	public String IterateValues(Model model) {
+		
+		List<String> names = new ArrayList<String>();
+		Collections.addAll(names, "manish", "sachin", "Payal", "Rohit");
+		System.out.println(names);
+		model.addAttribute("names", names);
+		
+		
+		return "iterate";	
+	}
+	
+	// handler for conditional statement
+	
+	@GetMapping("/ConditionalStatement")
+	public String condition(Model model) {
+		
+		model.addAttribute("isActive", false);
+		model.addAttribute("gender", "F");
+		
+		List<Integer> nums = List.of(12, 14, 16, 18, 20);
+		model.addAttribute("numslist", nums);
+	
+		return "condition";
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
